ac6ba02a361f66f2495dcc8023ea3dc85392a4fc
kartik <Kartikrajput77041@gmail.com>
2024-01-29 13:46:11 +0530

    Hosting on vercel

--- project_name/project_name/settings.py
+++ project_name/project_name/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-!e3@a5h1yh$2lv%o0=!lduhug4zrdq&upvxkh^5h2wu^2s^jd='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app','now.sh','127.0.0.1','localhost']
 
 
 # Application definition
